1ec2cfb8b27102c50e1989ec3fc130c69263433e
shaopengxu <shaopengxuiou@gmail.com>
2014-03-20 20:18:23 +0800

    Update and rename CommonUIFragment.java to HotQuestionFragment.java

--- src/com/xsp/themajority/HotQuestionFragment.java
+++ src/com/xsp/themajority/HotQuestionFragment.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class CommonUIFragment extends Fragment {
+public class HotQuestionFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
